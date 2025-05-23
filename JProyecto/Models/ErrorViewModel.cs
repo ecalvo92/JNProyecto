9763f12297cb704be6a0ff9e86db39b7c50ace22
ecalvo92 <ecalvo@crnova.com>
2025-05-22 20:15:46 -0600

    Commit Inicial

---
+++ JProyecto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JProyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
