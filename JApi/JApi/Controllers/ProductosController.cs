8a00e8422c6dc63a30a88e40a49263c3741e727a
ecalvo92 <ecalvo@crnova.com>
2025-08-07 20:32:06 -0600

    ActualizarProducto

--- JApi/JApi/Controllers/ProductosController.cs
+++ JApi/JApi/Controllers/ProductosController.cs
@@ -60,7 +60,50 @@ namespace JApi.Controllers
                 else
                     return BadRequest(_utilitarios.RespuestaIncorrecta("La información del producto no fue registrada"));
             }
-        }        
+        }
+
+        [HttpGet]
+        [Route("ConsultarProducto")]
+        public IActionResult ConsultarProducto(long IdProducto)
+        {
+            using (var context = new SqlConnection(_configuration.GetSection("ConnectionStrings:Connection").Value))
+            {
+                var resultado = context.QueryFirstOrDefault<Producto>("ConsultarProducto",
+                    new
+                    {
+                        IdProducto
+                    });
+
+                if (resultado != null)
+                    return Ok(_utilitarios.RespuestaCorrecta(resultado));
+                else
+                    return BadRequest(_utilitarios.RespuestaIncorrecta("No hay información registrada"));
+            }
+        }
+
+        [HttpPut]
+        [Route("ActualizarProducto")]
+        [Authorize]
+        public IActionResult ActualizarProducto(Producto producto)
+        {
+            using (var context = new SqlConnection(_configuration.GetSection("ConnectionStrings:Connection").Value))
+            {
+                var resultado = context.Execute("ActualizarProducto",
+                    new
+                    {
+                        producto.IdProducto,
+                        producto.Nombre,
+                        producto.Descripcion,
+                        producto.Precio,
+                        producto.Inventario
+                    });
+
+                if (resultado > 0)
+                    return Ok(_utilitarios.RespuestaCorrecta(null));
+                else
+                    return BadRequest(_utilitarios.RespuestaIncorrecta("La información del producto no fue actualizada"));
+            }
+        }
 
     }
 }
--- JProyecto/JProyecto/Controllers/ProductoController.cs
+++ JProyecto/JProyecto/Controllers/ProductoController.cs
@@ -69,10 +69,8 @@ namespace JProyecto.Controllers
                     var datos = resultado.Content.ReadFromJsonAsync<RespuestaEstandar<Producto>>().Result;
 
                     string archivo = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/productos", datos?.Contenido?.IdProducto + ext);
-                    using (var stream = new FileStream(archivo, FileMode.Create))
-                    {
-                        ImagenProducto.CopyTo(stream);
-                    }
+                    using var stream = new FileStream(archivo, FileMode.Create);
+                    ImagenProducto.CopyTo(stream);
 
                     return RedirectToAction("ConsultarProductos", "Producto");
                 }
@@ -86,15 +84,66 @@ namespace JProyecto.Controllers
         }
 
         [HttpGet]
-        public IActionResult ActualizarProducto()
+        public IActionResult ActualizarProducto(long q)
         {
-            return View();
+            using (var http = _http.CreateClient())
+            {
+                http.BaseAddress = new Uri(_configuration.GetSection("Start:ApiUrl").Value!);
+                http.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("JWT"));
+                var resultado = http.GetAsync("api/Productos/ConsultarProducto?IdProducto=" + q).Result;
+
+                if (resultado.IsSuccessStatusCode)
+                {
+                    var datos = resultado.Content.ReadFromJsonAsync<RespuestaEstandar<Producto>>().Result;
+                    return View(datos?.Contenido!);
+                }
+                else
+                {
+                    var respuesta = resultado.Content.ReadFromJsonAsync<RespuestaEstandar>().Result;
+                    ViewBag.Mensaje = respuesta?.Mensaje;
+                    return View();
+                }
+            }
         }
 
         [HttpPost]
-        public IActionResult ActualizarProducto(Producto producto)
+        public IActionResult ActualizarProducto(Producto producto, IFormFile ImagenProducto)
         {
-            return View();
+            if (ImagenProducto != null)
+            {
+                var ext = Path.GetExtension(ImagenProducto.FileName);
+                if (!ext.Equals(".png", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    ViewBag.Mensaje = "La imagen debe ser un archivo PNG.";
+                    return View();
+                }
+            }
+
+            using (var http = _http.CreateClient())
+            {
+                http.BaseAddress = new Uri(_configuration.GetSection("Start:ApiUrl").Value!);
+                http.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("JWT"));
+                var resultado = http.PutAsJsonAsync("api/Productos/ActualizarProducto", producto).Result;
+
+                if (resultado.IsSuccessStatusCode)
+                {
+                    if (ImagenProducto != null)
+                    {
+                        var ext = Path.GetExtension(ImagenProducto.FileName);
+                        string archivo = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/productos", producto.IdProducto + ext);
+                        using var stream = new FileStream(archivo, FileMode.Create);
+                        ImagenProducto.CopyTo(stream);
+                    }
+
+                    return RedirectToAction("ConsultarProductos", "Producto");
+                }
+                else
+                {
+                    var respuesta = resultado.Content.ReadFromJsonAsync<RespuestaEstandar>().Result;
+                    ViewBag.Mensaje = respuesta?.Mensaje;
+                    return View();
+                }
+            }
         }
 
     }
